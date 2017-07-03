b16be4028482f667930089c350dc8fb4ea5aec76
MakerLi <lihuanting.cn@gmail.com>
2017-07-03 14:16:52 +0800

    修改了数据库mysql与mysqli问题

--- common/comment/config.php
+++ common/comment/config.php
@@ -1,9 +1,13 @@
 <?php
-$host="localhost";
-$db_user="root";
-$db_pass="root";
-$db_name="chikefang";
-$link=mysql_connect($host,$db_user,$db_pass);
-mysql_select_db($db_name,$link);
+// $host="localhost";
+// $db_user="root";
+// $db_pass="root";
+// $db_name="chikefang";
+//老版本
+$link = @mysqli_connect( 
+ 'localhost', /* The host to connect to 连接MySQL地址 */  
+ 'root',   /* The user to connect as 连接MySQL用户名 */  
+ 'root', /* The password to use 连接MySQL密码 */  
+ 'chikefang');  /* The default database to query 连接数据库名称*/
 header("Content-Type: text/html; charset=utf-8");
 ?>
